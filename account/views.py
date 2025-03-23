1704d2b5202a83b891cdf2202cfcf5e0f4ece65e
friedjof@noweck.info <friedjof@noweck.info>
2025-03-23 21:57:25 +0100

    Add account-related templates, styles, and update settings for user authentication

--- account/views.py
+++ account/views.py
@@ -8,8 +8,23 @@ from .forms import RegisterForm, LoginForm, VerifyForm
 class LoginView(TemplateView):
     template_name = 'login.html'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['form'] = LoginForm()
+        return context
+
 class RegisterView(TemplateView):
     template_name = 'register.html'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['form'] = RegisterForm()
+        return context
+
 class VerifyView(TemplateView):
     template_name = 'verify.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['form'] = VerifyForm()
+        return context
