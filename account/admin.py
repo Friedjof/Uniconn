4f2eb43d917db85bf155b32c27689fde09751bd0
friedjof@noweck.info <friedjof@noweck.info>
2025-04-01 17:28:09 +0200

    Enhance admin interface for custom user model with improved fieldsets

--- account/admin.py
+++ account/admin.py
@@ -8,5 +8,12 @@ class CustomUserAdmin(UserAdmin):
     list_filter = ('role',)
     ordering = ('username',)
     search_fields = ('username', 'email')
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Persönliche Informationen', {'fields': ('first_name', 'last_name', 'email')}),
+        ('Berechtigungen', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Wichtige Daten', {'fields': ('last_login', 'date_joined')}),
+        ('Zusätzliche Informationen', {'fields': ('verification_code', 'role')}),
+    )
 
 admin.site.register(CustomUser, CustomUserAdmin)
