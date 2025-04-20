de7a07b5b16824bc327a6840792079dda4ff2507
BatuhanGQskt <e231529@gmail.com>
2025-04-21 00:22:37 +0200

    Add extension of the admin panel to import and export tables; Add registeries to admin.py for each table.

--- chat/admin.py
+++ chat/admin.py
@@ -1,6 +1,49 @@
 from django.contrib import admin
 from .models import Chat, Message
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
 
 # Register your models here.
-admin.site.register(Chat)
-admin.site.register(Message)
+class MessageResource(resources.ModelResource):
+    class Meta:
+        model = Message
+        fields = ('message_id', 'chat', 'sender', 'content', 'timestamp', 'status')
+        export_order = fields
+        import_id_fields = ('message_id',)
+
+@admin.register(Message)
+class MessageAdmin(ImportExportModelAdmin):
+    resource_class = MessageResource
+
+    list_display = ('message_id', 'chat', 'sender', 'content', 'timestamp', 'status')
+    list_filter = ('status',)
+    search_fields = ('message_id', 'chat__chat_id', 'sender__username', 'content')
+    ordering = ('timestamp',)
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        return qs.select_related('chat', 'sender')
+
+
+
+class ChatResource(resources.ModelResource):
+    class Meta:
+        model = Chat
+        fields = ('chat_id', 'status')
+        export_order = fields
+        import_id_fields = ('chat_id',)
+
+
+@admin.register(Chat)
+class ChatAdmin(ImportExportModelAdmin):
+    resource_class = ChatResource
+
+    list_display = ('chat_id', 'status', 'get_users')
+    list_filter = ('status',)
+    search_fields = ('chat_id',)
+    
+    def get_users(self, obj):
+        return ", ".join([user.username for user in obj.users.all()])
+    
+    get_users.short_description = 'Users'
+
