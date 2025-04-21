fa4401588ae492a2912ad4594b82a48426e4d234
BatuhanGQskt <e231529@gmail.com>
2025-04-22 01:38:32 +0200

    Implement search functionality; add search bar to homepage, create SearchResultsView, and handle user verification for search access

--- homepage/urls.py
+++ homepage/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import HomePageView
+from .views import HomePageView, SearchResultsView
 
 app_name = 'homepage'
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='index'),
+    path('search/', SearchResultsView.as_view(), name='search_results'),
 ]
